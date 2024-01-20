4c182f73eff45fc2445b1cd147c4910518fdfa88
Kerim <kerim.sabic.207@gmail.com>
2024-01-20 11:15:30 +0100

    more cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import {  useState } from "react"
 import {  Route, Routes } from "react-router-dom"
-
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
 import Members from "./pages/Members"
