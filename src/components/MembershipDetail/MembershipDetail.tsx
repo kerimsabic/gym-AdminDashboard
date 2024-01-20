789a648a8c12805b930fe3e0fadaf57f2154fa58
Kerim <kerim.sabic.207@gmail.com>
2024-01-20 23:16:59 +0100

    more fixes

--- src/components/MembershipDetail/MembershipDetail.tsx
+++ src/components/MembershipDetail/MembershipDetail.tsx
@@ -107,7 +107,7 @@ const MembershipDetail = ({ selectedUserId, closeMembershipDetail, onSubmitForm 
                     <div className="mb-5">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Memberhsip End Date</label>
                         <div id="large-input" className="text-center block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
-                        {membership?.startDate ? new Date(membership.startDate).toLocaleDateString() : 'N/A'}
+                        {membership?.endDate ? new Date(membership.endDate).toLocaleDateString() : 'N/A'}
                         </div>
                     </div>
                 </div>
