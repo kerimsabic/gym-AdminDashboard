2b5fb6368c801945cf1cc1c590a0e6a0eacad26c
Kerim <kerim.sabic.207@gmail.com>
2024-04-21 13:25:03 +0200

    login color

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -86,7 +86,7 @@ const LogIn = () => {
 
                     Gym
                 </a>
-                <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
+                <div className="w-full bg-[#f0f8ff] rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                             Sign in to your account
